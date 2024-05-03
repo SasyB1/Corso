d5c8a2fd5b2baf586acc70855bf9a0bc8e19c3d8
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-03 17:10:07 +0200

    1.0.0

---
+++ 2 unita/2 settimana/5 giorno/assets/js/firstFile.js
@@ -0,0 +1,39 @@
+const cardAccounts = document.getElementsByClassName("cardAccount");
+const imgAccounts = document.getElementsByClassName("imgAccount");
+const titleAccounts = document.getElementsByClassName("titleAccount");
+
+Array.from(cardAccounts).forEach((element, i) => {
+  element.addEventListener("mouseover", () => {
+    imgAccounts[i].classList.add("borderImageAccount");
+    titleAccounts[i].classList.add("colorTitleAccount");
+  });
+});
+
+Array.from(cardAccounts).forEach((element, i) => {
+  element.addEventListener("mouseout", () => {
+    imgAccounts[i].classList.remove("borderImageAccount");
+    titleAccounts[i].classList.remove("colorTitleAccount");
+  });
+});
+
+let addedIcons = true;
+
+const buttonFirstPage = document.querySelector(".buttonFirstPage");
+const buttonEdit = document.querySelectorAll(".buttonEdit");
+
+buttonFirstPage.addEventListener("click", function () {
+  if (addedIcons) {
+    buttonEdit.forEach((button) => {
+      const imageEdit = document.createElement("i");
+      imageEdit.classList.add("bi", "bi-pencil", "editFirstImage", "editIcon");
+      button.appendChild(imageEdit);
+    });
+    addedIcons = false;
+  } else {
+    const editIcons = document.querySelectorAll(".editIcon");
+    editIcons.forEach((icon) => {
+      icon.remove();
+    });
+    addedIcons = true;
+  }
+});
