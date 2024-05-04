f4774cd9cd628adbb0ee63826b17b30a29bc0423
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-04 16:08:32 +0200

    1.4.1

--- 2 unita/2 settimana/5 giorno/assets/js/homePage.js
+++ 2 unita/2 settimana/5 giorno/assets/js/homePage.js
@@ -3,17 +3,13 @@ const imgCar = document.querySelectorAll("img");
 imgCar.forEach((img) => {
   img.addEventListener("mouseover", () => {
     const carouselInner = img.closest(".carousel-inner");
-    if (carouselInner) {
-      carouselInner.classList.add("carusielVisible");
-    }
+    carouselInner.classList.add("carusielVisible");
   });
 });
 
 imgCar.forEach((img) => {
   img.addEventListener("mouseout", () => {
     const carouselInner = img.closest(".carousel-inner");
-    if (carouselInner) {
-      carouselInner.classList.remove("carusielVisible");
-    }
+    carouselInner.classList.remove("carusielVisible");
   });
 });
