d09473509ffa20cad3628e5fdfa56054d488bfe5
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-05 09:16:59 +0200

    1.6.0

--- 2 unita/2 settimana/5 giorno/assets/js/homePage.js
+++ 2 unita/2 settimana/5 giorno/assets/js/homePage.js
@@ -1,3 +1,48 @@
+// ------------------- REFRESH LOGO ----------------------------
+const refreshLogo = document.querySelector("header img");
+refreshLogo.addEventListener("click", function () {
+  location.reload();
+});
+
+// ------------------- NAV BAR ----------------------------
+
+const navbarNav = document.querySelectorAll(".navbar-nav li a");
+
+navbarNav.forEach((navGenere) => {
+  navGenere.addEventListener("mouseover", () => {
+    navGenere.classList.add("navFocus");
+  });
+});
+navbarNav.forEach((navGenere) => {
+  navGenere.addEventListener("mouseout", () => {
+    navGenere.classList.remove("navFocus");
+  });
+});
+
+const searchIcon = document.querySelector(".bi.bi-search");
+const searchInput = document.querySelector(".bi.bi-search input");
+
+searchIcon.addEventListener("click", () => {
+  searchInput.classList.add("navSearch");
+});
+
+const homepageOptions = document.querySelector(
+  ".dropdown-menu.bg-netflixSecond"
+);
+const profileNavigation = homepageOptions.querySelector("li:first-child");
+const settingsNavigation = homepageOptions.querySelector("li:nth-child(2)");
+const changeProfileNavigation =
+  homepageOptions.querySelector("li:nth-child(3)");
+
+profileNavigation.addEventListener("click", () => {
+  window.location.href = "modificaProfilo.html";
+});
+
+changeProfileNavigation.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+// ------------------- CAROUSEL ----------------------------
 const imgCar = document.querySelectorAll("img");
 
 imgCar.forEach((img) => {
