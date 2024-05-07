4f90698cfbcd7b39aff093336fa9fb548102cefa
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-07 15:08:56 +0200

    07-05

--- 2 unita/3 settimana/1 giorno/contact-list.js
+++ 2 unita/3 settimana/1 giorno/contact-list.js
@@ -1,57 +1,57 @@
 // utilizziamo il form per creare dei contatti
 // prima cosa: prendiamo i riferimenti nel DOM dei vari input
 // campo nome
-const nameInput = document.getElementById('name')
+const nameInput = document.getElementById("name");
 // campo telefono
-const phoneInput = document.getElementById('phone')
+const phoneInput = document.getElementById("phone");
 // checkbox preferiti
-const favCheckbox = document.getElementById('favourite')
+const favCheckbox = document.getElementById("favourite");
 // riferimento generico al form
-const formTag = document.getElementsByTagName('form')[0]
+const formTag = document.getElementsByTagName("form")[0];
 
 // creiamo ora l'array che memorizzerà i nostri contatti
-const contacts = []
+const contacts = [];
 
 // classe Contact
 class Contact {
   constructor(_name, _phone, _fav) {
-    this.name = _name
-    this.phone = _phone
-    this.fav = _fav
+    this.name = _name;
+    this.phone = _phone;
+    this.fav = _fav;
   }
 }
 
 const updateContacts = function () {
   // prendo un riferimento alla riga dove sono contenute le colonne
-  const contactsRow = document.getElementById('contacts-row')
+  const contactsRow = document.getElementById("contacts-row");
   // prima della creazione delle cards dovrà svuotare i contenuti esistenti
-  contactsRow.innerHTML = ''
+  contactsRow.innerHTML = "";
   // adesso, per ogni elemento di contacts, creo una colonna con dentro il contatto
   contacts.forEach((contact) => {
     // cosa faccio con ogni contatto?
     // creo un div vuoto
-    const newDiv = document.createElement('div') // <div></div>
+    const newDiv = document.createElement("div"); // <div></div>
     // rendiamo questo div una col di bootstrap
-    newDiv.classList.add('col')
+    newDiv.classList.add("col");
     // riempio newDiv con una card
     newDiv.innerHTML = `
         <div class="card${
-          contact.fav ? ' border border-success border-3' : ''
+          contact.fav ? " border border-success border-3" : ""
         }">
             <div class="card-body">
                 <h5 class="card-title">${contact.name}</h5>
                 <h5 class="card-title">${contact.phone}</h5>
             </div>
         </div>
-    `
+    `;
     // appendo ora la col alla row
-    contactsRow.appendChild(newDiv)
-  })
-}
+    contactsRow.appendChild(newDiv);
+  });
+};
 
 // ora occupiamoci dell'evento di submit del form
-formTag.addEventListener('submit', function (e) {
-  e.preventDefault() // NON aggiornare la pagina e perdere i dati
+formTag.addEventListener("submit", function (e) {
+  e.preventDefault(); // NON aggiornare la pagina e perdere i dati
   // ora posso scrivere le mie istruzioni
   // voglio creare un oggetto a partire da una classe
   // con i dati provenienti dal form
@@ -60,17 +60,17 @@ formTag.addEventListener('submit', function (e) {
     phoneInput.value,
     favCheckbox.checked
     // "checked" nelle checkboxes è true/false
-  )
+  );
 
   // aggiungere contactFromFormValues ai contatti correnti
-  contacts.push(contactFromFormValues)
+  contacts.push(contactFromFormValues);
 
-  console.log('CONTATTO CREATO', contactFromFormValues)
+  console.log("CONTATTO CREATO", contactFromFormValues);
   // azzeriamo il form
-  nameInput.value = ''
-  phoneInput.value = ''
-  favCheckbox.checked = false
+  nameInput.value = "";
+  phoneInput.value = "";
+  favCheckbox.checked = false;
 
   // aggiorniamo la riga con le colonne con un nuovo elemento per il mio nuovo contatto
-  updateContacts()
-})
+  updateContacts();
+});
---
+++ 2 unita/3 settimana/2 giorno/07-05/script.js
@@ -0,0 +1,31 @@
+const formTag = document.getElementsByTagName("form")[0];
+const inputText = document.querySelector(".form-control");
+const titleForm = document.querySelector(".titleForm");
+const buttonSave = document.querySelector(".border-success");
+const buttonDelete = document.querySelector(".border-danger");
+
+formTag.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+buttonSave.addEventListener("click", () => {
+  const inputValue = inputText.value;
+  localStorage.setItem("nomeUtente", inputValue);
+  titleForm.innerHTML = inputValue;
+  inputText.value = "";
+});
+
+buttonDelete.addEventListener("click", () => {
+  localStorage.removeItem("nomeUtente");
+  titleForm.innerHTML = "Registrati";
+  inputText.value = "";
+});
+
+const counterElement = document.querySelector("#counter");
+let counter = 0;
+
+const timerInterval = setInterval(() => {
+  counter++;
+  counterElement.innerText = counter;
+  sessionStorage.setItem("counter", counter);
+}, 1000);
